335a752cd2cad82966f979ef24b8670fa01ec381
jamesrayford <32623173+jamesrayford@users.noreply.github.com>
2018-03-07 21:17:41 -0500

    Initial-Commit

---
+++ Template/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        while (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        //while (Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(Vector2.left * speed * Time.deltaTime);
+        //}
+        //while (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    transform.Translate(Vector2.up * speed * Time.deltaTime);
+        //}
+        //while (Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    transform.Translate(Vector2.down * speed * Time.deltaTime);
+        //}
+    }
+}
